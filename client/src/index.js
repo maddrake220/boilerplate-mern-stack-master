a2035bb2fa76d5de43683cc109e7ef352b2b2807
maddrake220 <83238246+maddrake220@users.noreply.github.com>
2021-05-28 19:34:57 +0900

    heroku deployment

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import Reducer from './_reducers';
 import { Provider } from 'react-redux';
@@ -25,9 +25,9 @@ ReactDOM.render(
             window.__REDUX_DEVTOOLS_EXTENSION__()
         )}
     >
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
     , document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
