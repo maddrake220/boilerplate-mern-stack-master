3c2c6e350d766b7f051b3b1dfab4a5dc59882887
maddrake220 <83238246+maddrake220@users.noreply.github.com>
2021-06-03 05:46:08 +0900

    browserrouter

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import Reducer from './_reducers';
 import { Provider } from 'react-redux';
@@ -25,9 +25,9 @@ ReactDOM.render(
             window.__REDUX_DEVTOOLS_EXTENSION__()
         )}
     >
-        <HashRouter>
+        <BrowserRouter>
             <App />
-        </HashRouter>
+        </BrowserRouter>
     </Provider>
     , document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
